16159fd47d56161856d067ad349a06831f1a78f5
Wendelsena <wendel4019@gmail.com>
2024-06-24 15:53:30 -0300

    Criado Spring project base

---
+++ professional/src/main/java/com/register/backend/ProfessionalApplication.java
@@ -0,0 +1,13 @@
+package com.register.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalApplication.class, args);
+	}
+
+}
---
+++ professional/src/test/java/com/register/professional/ProfessionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.register.professional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
